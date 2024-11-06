cfc3ca56dffc9aca544001286720d552ec259822
denise030261 <83278071+denise030261@users.noreply.github.com>
2024-11-06 13:15:55 +0900

    [Silver III] Title: 최고의 피자, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int a, int b)
+{
+	return a > b;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, A, B, C;
+	vector<int> D;
+	long long int answer = 0;
+	cin >> N;
+	cin >> A >> B;
+	cin >> C;
+
+	int toppings = 0;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> toppings;
+		D.push_back(toppings);
+	}
+	
+	sort(D.begin(), D.end(), compare);
+
+	int sumToppings = 0;
+	int sumToppingsCost = 0;
+	answer = C / A;
+	for (int i = 0; i < D.size(); i++)
+	{
+		sumToppingsCost += B;
+		sumToppings += D[i];
+		long long int calorie = (C + sumToppings) / (A + sumToppingsCost);
+		if (answer <= calorie)
+		{
+			answer = calorie;
+		}
+		else
+		{
+			break;
+		}
+	}
+	cout << answer;
+}
