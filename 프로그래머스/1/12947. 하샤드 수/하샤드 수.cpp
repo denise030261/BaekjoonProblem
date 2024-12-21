1af494b40291b8b446bf45c59493d8c78b2c9c70
denise030261 <83278071+denise030261@users.noreply.github.com>
2024-12-21 22:21:17 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int sum=0;
+    int num=x;
+    while(x!=0)
+    {
+        sum+=x%10;
+        x/=10;
+    }
+    
+    if(num%sum==0)
+    {
+        answer=true;
+    }
+    else
+    {
+        answer=false;
+    }
+    return answer;
+}
