055e9270d9069a22b2a66702547b84428de84457
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-01-10 23:26:24 +0900

    [Gold V] Title: 캠프 준비, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int N, L, R, X;
+vector<int> v;
+int answer = 0;
+bool visited[20] = { 0, };
+
+void dfs(int start, long long int sum, int minBound,int maxBound)
+{
+	for (int i = start; i < N; i++)
+	{
+		if (visited[i])
+			continue;
+
+
+		if (v[i] + sum >= L && v[i] + sum <= R
+			&& max(maxBound, v[i]) - min(minBound, v[i]) >= X)
+			answer++;
+
+		visited[i] = true;
+		dfs(i, sum + v[i], min(minBound, v[i]), max(maxBound, v[i]));
+		visited[i] = false;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> N >> L >> R >> X;
+	int num;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+
+	dfs(0,0,1000000, 1);
+
+	cout << answer;
+}
