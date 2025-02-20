b2abd7f6a9f162fb7155389b4e3bdc2854686a82
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-02-21 00:29:28 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int year;
+    cin >> year;
+    cout << year-1946;
+}
