cc14466d1042419780d80669fb034f8e9f7ad9ff
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-03-08 01:20:00 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int A, B, C;
+	cin >> A >> B >> C;
+	if (A + B == C)
+		cout << "correct!";
+	else
+		cout << "wrong!";
+}
