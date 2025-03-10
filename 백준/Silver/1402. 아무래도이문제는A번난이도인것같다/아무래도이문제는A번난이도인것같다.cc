39d8713bf1aaedda65ea8e2f78028f62ca38a43e
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-03-11 03:33:02 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int T, A, B;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> A >> B;
+		cout << "yes" << '\n';
+	}
+}
