bbe762e2ed7371dcaa2e7b1a055f254a1b1abceb
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-03-29 22:53:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	string str;
+	cin >> str;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << str << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+}
