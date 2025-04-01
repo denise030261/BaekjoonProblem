9b140f5cd169a64f743634d22e887550a4835acf
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-04-01 12:14:09 +0900

    [Gold IV] Title: LCS 3, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.cc
+++ 백준/Gold/1958. LCS 3/LCS 3.cc
@@ -1,58 +1,12 @@
 #include <iostream>
-#include<algorithm> 
-#include<string>
-#include <cstring>
 
 using namespace std;
-
-int dp[101][101][101] = {0,};
-
-//string LCS(string str1, string str2)
-//{
-//	string str = "";
-//	for (int i = 1; i <= str1.length(); i++)
-//	{
-//		for (int j = 1; j <= str2.length(); j++)
-//		{
-//			if (str1[i - 1] == str2[j - 1])
-//			{
-//				dp[i][j] = dp[i - 1][j - 1] + 1;
-//			}
-//			else
-//			{
-//				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-//			}
-//		}
-//	}
-//
-//	int i = str1.size();
-//	int j = str2.size();
-//	while (i > 0 && j > 0)
-//	{
-//		if (str1[i - 1] == str2[j - 1])
-//		{
-//			str += str1[i - 1];
-//			i--;
-//			j--;
-//		}
-//		else if (dp[i - 1][j] > dp[i][j - 1])
-//		{
-//			i--;
-//		}
-//		else
-//		{
-//			j--;
-//		}
-//	}
-//
-//	reverse(str.begin(), str.end());
-//	return str;
-//}
 
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	string str1, str2, str3, str4, str5;
+	string str1, str2, str3;
+	int dp[101][101][101] = { 0, };
 
 	cin >> str1 >> str2 >> str3;
 
