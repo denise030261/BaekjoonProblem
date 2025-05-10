c8a3a3c7b8ef4f545704c526bb8f12819961fd50
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-05-10 13:04:59 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 44 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.cc
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.cc
@@ -10,8 +10,7 @@ int main()
 	int N, M;
 	cin >> N >> M;
 	int origin[100001] = { 0, };
-	int starts[100001] = { 0, };
-	int ends[100001] = { 0, };
+	int sum[100001] = { 0, };
 
 	for (int i = 0; i < N; i++)
 	{
@@ -22,24 +21,15 @@ int main()
 		int start, end, cost;
 		cin >> start >> end >> cost;
 
-		starts[start-1] += cost;
-		ends[end-1] += cost;
+		sum[start-1] += cost;
+		sum[end] -= cost;
 	}
 
 	int add = 0;
 	for (int i = 0; i < N; i++)
 	{
-		if (starts[i] != 0)
-		{
-			add += starts[i];
-		}
-		origin[i] += add;
-		if (ends[i] != 0)
-		{
-			add -= ends[i];
-		}
-		cout << origin[i] << ' ';
+		add += sum[i];
+		cout << origin[i] + add << ' ';
 	}
-
 
 }
