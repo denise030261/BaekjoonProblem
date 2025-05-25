f98d9f9cc6767bf72ad6c29ebfbf5ae55a2fdefa
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-05-26 03:44:25 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n1, k1, n2, k2;
+	cin >> n1 >> k1 >> n2 >> k2;
+	cout << n1 * k1 + n2 * k2;
+}
