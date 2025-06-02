c908970c8451b9282bc86c1fccc04c210a952139
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-06-02 13:43:25 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 8 ms, Memory: 9732 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n, m;
+	int answer = 0;
+	int dp[1001][1001] = { 0, };
+	int map[1001][1001];
+	cin >> n >> m;
+
+	string str;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> str;
+		for (int j = 1; j <= m; j++)
+		{
+			map[i][j] = str[j-1] - '0';
+		}
+	}
+	
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			if (map[i][j] == 1)
+			{
+				dp[i][j] = 1;
+				
+				if (map[i - 1][j] == 1 && map[i][j - 1] == 1 && map[i - 1][j - 1] == 1)
+				{
+					dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 1);
+				}
+			}
+
+			answer = max(answer, dp[i][j]);
+		}
+	}
+
+	/*for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cout << dp[i][j] << ' ';
+		}
+		cout << '\n';
+	}*/
+	
+	cout << answer * answer;
+}
