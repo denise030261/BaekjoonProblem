9dd14c3a0b0f2280c64f733c8665170877a19ecf
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-06-16 21:45:05 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int A, B;
+	cin >> A >> B;
+	cout << abs(A - B);
+}
