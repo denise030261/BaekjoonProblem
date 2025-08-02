957a43bc1f673efb08ee2d375b8eb5912689d3f6
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-08-02 22:10:22 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5812 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, K;
+	cin >> N >> K;
+
+	int dp[1001][1001] = { 0, };
+
+	for (int i = 1; i <= N; i++)
+	{
+		dp[i][1] = i;
+		dp[i][i] = 1;
+		dp[i][0] = 1;
+	}
+
+	for (int i = 3; i <= N; i++)
+	{
+		for (int j = 2; j <= i; j++)
+		{
+			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+		}
+	}
+
+	cout << dp[N][K];
+}
