d6c6a649f3efd081777bb68025d5327587e1da27
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-08-17 14:33:23 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 28 ms, Memory: 5812 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N;
+	int dp[1000][1000] = { 0, };
+	int matrix[1000][2] = { 0, };
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int r,c;
+		cin >> r>>c;
+		matrix[i+1][0] = r;
+		matrix[i+1][1] = c;
+	}
+
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 1; i+j <= N; j++)
+		{
+			dp[j][i + j] = INT_MAX;
+			for (int k = j; k <= i+j; k++)
+			{
+				dp[j][i+j] = min(dp[j][i+j], dp[j][k] + dp[k + 1][i+j] + matrix[j][0] * matrix[k][1] * matrix[i + j][1]);
+			}
+		}
+	}
+
+	cout << dp[1][N];
+}
