193975d405bf38642e3a2144ea44a7c2d29fd39a
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-08-22 22:20:47 +0900

    [Gold III] Title: 크게 만들기, Time: 8 ms, Memory: 4020 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, M;
+	vector<int> q[10];
+	stack<int> st;
+	string str;
+	cin >> N >> M;
+	cin >> str;
+	
+	for (int i = 0; i<str.length(); i++)
+	{
+		int num = str[i] - '0';
+		
+		if (st.empty() || M == 0 || (!st.empty() && st.top()>=num))
+		{
+			st.push(num);
+		}
+		else if (st.top() < num && !st.empty())
+		{
+			while(!st.empty() && M!=0)
+			{
+				if (st.top() < num)
+				{
+					st.pop();
+					M--;
+				}
+				else
+				{
+					break;
+				}
+			}
+			st.push(num);
+		}
+	}
+
+	while (M != 0)
+	{
+		st.pop();
+		M--;
+	}
+
+	string answer = "";
+	while (!st.empty())
+	{
+		answer += st.top() + '0';
+		st.pop();
+	}
+
+	reverse(answer.begin(), answer.end());
+
+	if (answer == "")
+	{
+		cout << 0;
+	}
+	else
+	{
+		cout << answer;
+	}
+}
