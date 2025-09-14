ef40abdc507dab5e32a6acbb54d671a8e2ff6ce0
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-09-15 00:50:56 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N;
+	vector<int> v;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	if (next_permutation(v.begin(), v.end()))
+	{
+		for (int i = 0; i < N; i++)
+		{
+			cout << v[i] << ' ';
+		}
+	}
+	else
+	{
+		cout << -1;
+	}
+}
