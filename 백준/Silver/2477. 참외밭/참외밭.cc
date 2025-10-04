8c411c074b772cca5889e07adbb56692f1c2bc4f
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-10-04 23:08:56 +0900

    [Silver II] Title: 참외밭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int K; // 참외의 개수
+	int maxHeight = 0, minHeight = INT_MAX;
+	int maxWidth = 0, minWidth = INT_MAX;
+	vector<pair<int, int>> v;
+
+	cin >> K;
+	for (int i = 0; i < 6; i++)
+	{
+		int num, dist;
+		cin >> num >> dist;
+
+		if (num == 1 || num == 2)
+		{
+			maxWidth = max(maxWidth, dist);
+			minWidth = min(minWidth, dist);
+		}
+		else
+		{
+			maxHeight = max(maxHeight, dist);
+			minHeight = min(minHeight, dist);
+		}
+		v.push_back({ num,dist });
+	}
+
+	int area = maxHeight * maxWidth;
+	int partArea = 0;
+	for (int i = 0; i < 6; i++)
+	{
+		int curA = i;
+		int nextA = (i + 2) % 6;
+		int curB = (i + 1) % 6;
+		int nextB = (i + 3) % 6;
+
+		if (v[curA].first == v[nextA].first && v[curB].first == v[nextB].first)
+		{
+			partArea = v[nextA].second * v[curB].second;
+			break;
+		}
+	}
+	cout << (area-partArea) * K;
+}
