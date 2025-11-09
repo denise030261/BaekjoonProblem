6cc08093256b26f7df76e4b6904456e997c2fe11
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-11-10 00:06:01 +0900

    [Silver I] Title: 창고 다각형, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N;
+	int arr[1001] = { 0, };
+	cin >> N;
+	int start = INT_MAX, end = -1;
+	vector<pair<int, int>> v;
+
+	for (int i = 0; i < N; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		arr[a] = b;
+		v.push_back({ a,b });
+		
+		start = min(start, a);
+		end = max(end, a);
+	}
+	
+	int answer = INT_MAX;
+	for (int i = 0; i < N; i++)
+	{
+		//cout << "------------------------------------------------------\n";
+		int sum = 0;
+		bool correct = true;
+		int height = -1;
+		for (int j = start; j < v[i].first; j++)
+		{
+			if (arr[j] != 0 && height < arr[j])
+			{
+				height = arr[j];
+			}
+			sum += height;
+			//cout << height << ' ' << sum << '\n';
+		}
+
+		if (height > v[i].second)
+		{
+			correct = false;
+		}
+
+		height = -1;
+		for (int j = end; j >= v[i].first+1; j--)
+		{
+			if (arr[j] != 0 && height < arr[j])
+			{
+				height = arr[j];
+			}
+			sum += height;
+			//cout << height << ' ' << sum << '\n';
+		}
+		if (height > v[i].second)
+		{
+			correct = false;
+		}
+
+		if(correct)
+		{
+			answer = min(answer, sum+v[i].second);
+			//cout << "answer is " << answer << '\n';
+		}
+	}
+
+	cout << answer;
+}
