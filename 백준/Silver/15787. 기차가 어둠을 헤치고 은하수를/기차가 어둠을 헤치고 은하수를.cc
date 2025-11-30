21fc166b82d7a2d91bee720ab9e06ab87372cc0c
denise030261 <83278071+denise030261@users.noreply.github.com>
2025-11-30 19:13:40 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 16 ms, Memory: 2424 KB -BaekjoonHub

---
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.cc
@@ -0,0 +1,54 @@
+#include <iostream> 
+#include <map> 
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, M;
+    int arr[100001] = { 0, };
+    map<int, int> answer;
+    
+    cin >> N >> M;
+    for (int i = 0; i < M; i++)
+    {
+        int order, train;
+        cin >> order >> train;
+        if (order == 1 || order == 2)
+        {
+            int seat;
+            cin >> seat;
+
+            if (order == 1)
+            {
+                arr[train] |= (1 << seat);
+            }
+            else
+            {
+                arr[train] &= ~(1 << seat);
+            }
+            //cout << train << " : " << arr[train] << '\n';
+        }
+        else if(order==3)
+        {
+            arr[train] <<= 1;
+            arr[train] &= ~(1 << 21);
+        }
+        else
+        {
+            arr[train] >>= 1;
+            arr[train] &= ~(1 << 0);
+        }
+
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        //cout << i << " : " << arr[i] << '\n';
+        answer[arr[i]]++;
+    }
+
+    cout << answer.size();
+}
