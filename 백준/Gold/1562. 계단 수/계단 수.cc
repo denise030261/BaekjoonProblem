e79652a5b41f81e1b9224226c64b0e3a00ea27e4
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-01-04 15:32:07 +0900

    [Gold I] Title: 계단 수, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N;
+    int dp[101][10][1024] = { 0, };
+    cin >> N;
+
+    for (int i = 1; i <= 9; i++)
+    {
+        dp[1][i][1 << i] = 1;
+    }
+
+    for (int i = 2; i <= N; i++)
+    {
+        for (int j = 0; j <= 9; j++)
+        {
+            if (i == 1 && j == 0)
+            {
+                continue;
+            }
+
+            for (int k = 0; k < (1<<10); k++)
+            {
+                if (j == 0)
+                {
+                    dp[i][j][k | (1 << j)] += dp[i - 1][1][k];
+                }
+                else if (j == 9)
+                {
+                    dp[i][j][k | (1 << j)] += dp[i - 1][8][k];
+                }
+                else
+                {
+                    dp[i][j][k | (1 << j)] += dp[i - 1][j-1][k];
+                    dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
+                }
+                dp[i][j][k | (1 << j)] %= 1000000000;
+            }
+        }
+    }
+
+    long long int answer = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        answer += dp[N][i][1023];
+        answer %= 1000000000;
+    }
+    cout << answer;
+}
