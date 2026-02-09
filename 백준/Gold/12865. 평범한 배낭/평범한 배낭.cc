32e49b2833ab4e9697efbc7646058fb030510926
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-02-09 16:23:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 41352 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,29 +1,47 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <queue>
+#include <climits>
 
 using namespace std;
 
-int dp[101][100001];
-
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, K;
+    int dp[101][100001] = { 0, };
+    cin >> N >> K;
 
-	int N, K, W, V;
-	cin >> N >> K;
+    for (int i = 1; i <= N; i++)
+    {
+        int W, V;
+        cin >> W >> V;
+        for (int j = 1; j <= K; j++)
+        {
+            if (j - W >= 0)
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W] + V);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
 
-	for (int i = 1; i <= N; i++)
-	{
-		cin >> W >> V;
+    /*int answer = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j <= K; j++)
+        {
+            cout << dp[i][j] << ' ';
+            answer = max(dp[i][j], answer);
+        }
 
-		for (int j = 1; j <= K; j++)
-		{
-			if (j >= W)
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W] + V);
-			else
-				dp[i][j] = dp[i - 1][j];
-		}
-	}
+        cout << '\n';
+    }
 
-	cout << dp[N][K];
+    cout << answer;*/
+    cout << dp[N][K];
 }
