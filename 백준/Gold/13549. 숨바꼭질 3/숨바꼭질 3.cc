dc3e00d44559cdc61abb27d4ef01c038749424ec
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-02-12 19:33:42 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 2988 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,48 +1,49 @@
 #include <iostream>
+#include <climits>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, K;
+    cin >> N >> K;
+    bool visited[200001] = { 0, };
+    int answer = INT_MAX;
 
-	int start, target,location,second;
-	int answer=9999999;
-	bool visited[100001] = {0,};
-	queue<pair<int, int>> q;
+    priority_queue<pair<int, int>> pq;
+    pq.push({ 0,N });
 
-	cin >> start >> target;
+    while (!pq.empty())
+    {
+        int cost = -pq.top().first;
+        int current = pq.top().second;
+        pq.pop();
+        //cout << cost << ' ' << current << '\n';
 
-	q.push(pair<int, int>(start, 0));
+        if (current == K)
+        {
+            cout << cost;
+            break;
+        }
 
-	while (!q.empty())
-	{
-		location = q.front().first;
-		second = q.front().second;
-		q.pop();
-		visited[location] = true;
+        if (visited[current])
+        {
+            continue;
+        }
 
-		if (location==target)
-		{
-			answer = min(second,answer);
-		}
-		if (location * 2 < 100001)
-		{
-			if(!visited[location * 2])
-				q.push(pair<int, int>(location * 2, second));
-		}
-		if (location + 1 < 100001)
-		{
-			if(!visited[location + 1])
-				q.push(pair<int, int>(location + 1, second + 1));
-		}
-		if (location - 1 >= 0)
-		{
-			if(!visited[location - 1])
-				q.push(pair<int, int>(location - 1, second + 1));
-		}
-	}
+        visited[current] = true;
 
-	cout << answer;
+        if (current < K)
+        {
+            pq.push({ -(cost + 1),current + 1 });
+            pq.push({ -cost,current * 2 });
+        }
+        if(current-1>=0)
+        {
+            pq.push({ -(cost + 1),current - 1 });
+        }
+    }
 }
