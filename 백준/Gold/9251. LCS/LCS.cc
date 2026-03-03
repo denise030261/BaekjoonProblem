02a2cf38e3394b471f7bb7d313df4b79d043016f
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-03 11:22:32 +0900

    [Gold IV] Title: LCS, Time: 4 ms, Memory: 5808 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,33 +1,47 @@
 #include <iostream>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    string str1, str2;
+    int dp[1000][1000] = { 0, };
+    cin >> str1 >> str2;
 
-	int arr[1001][1001] = {0,};
-	string str1;
-	string str2;
+    for (int i = 0; i < str1.length(); i++)
+    {
+        int num = 0;
+        for (int j = 0; j < str2.length(); j++)
+        {
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                num = max(num, dp[i - 1][j - 1]);
+            }
 
-	cin >> str1 >> str2;
+            if (str1[i] == str2[j])
+            {
+                if (i == 0 || j==0)
+                {
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    dp[i][j] += num + 1;
+                }
+            }
+            else if (i != 0)
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
 
-	for (int i = 1; i <= str1.length(); i++)
-	{
-		for (int j = 1; j <= str2.length(); j++)
-		{
-			if (str1[i-1] == str2[j-1])
-			{
-				arr[i][j] = arr[i - 1][j - 1] + 1;
-			}
-			else
-
-			{
-				arr[i][j] = max(arr[i - 1][j], arr[i][j - 1]);
-			}
-		}
-	}
-
-	cout << arr[str1.length()][str2.length()];
+    int answer = 0;
+    for (int i = 0; i < str2.length(); i++)
+    {
+        answer = max(answer, dp[str1.length() - 1][i]);
+    }
+    cout << answer;
 }
