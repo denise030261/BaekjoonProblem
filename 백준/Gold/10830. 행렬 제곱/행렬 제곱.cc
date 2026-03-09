6c1f96e3a9dd8fa72240902d7541e1590c217b60
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-09 14:38:01 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -1,67 +1,104 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 long long int N, B;
-long long int A[6][6];
-long long int answer[6][6];
-long long int temp[6][6];
+int arr[5][5] = { 0, };
 
-void Matrix(long long int mat1[6][6], long long int mat2[6][6])
+void calculate(int A[5][5], int B[5][5], int C[5][5])
 {
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				temp[i][j] = 0;
-				for (int k = 1; k <= N; k++)
-				{
-					temp[i][j] += mat1[i][k] * mat2[k][j];
-				}
-				temp[i][j] %=  1000;
-			}
-		}
+    int temp[5][5] = { 0, };
 
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				mat1[i][j] = temp[i][j];
-			}
-		}
+    /*for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << A[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << B[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';*/
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < N; k++)
+            {
+                sum += A[i][k] * B[k][j];
+            }
+            temp[i][j] = sum;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            C[i][j] = temp[i][j]%1000;
+            //cout << C[i][j] << ' ';
+        }
+        //cout << '\n';
+    }
+    //cout << '\n';
+}
+
+void divAndConq(long long int cur, int cal[5][5])
+{
+    if (cur == 1)
+    {
+        //cout << "first : \n";
+        calculate(arr, cal, cal);
+        return;
+    }
+
+    divAndConq(cur / 2, cal);
+
+    //cout << "calculate : \n";
+    calculate(cal, cal, cal);
+    if (cur % 2 == 1)
+    {
+        //cout << "odd : \n";
+        calculate(cal, arr, cal);
+    }
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> B;
+    int answer[5][5] = { 0, };
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> arr[i][j];
 
-	cin >> N >> B;
+            if(i==j)
+                answer[i][j] = 1;
+        }
+    }
 
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			cin >> A[i][j];
-		}
-		answer[i][i] = 1;
-	}
+    divAndConq(B, answer);
 
-	while (B)
-	{
-		if (B % 2 == 1)
-		{
-			Matrix(answer, A);
-		}
-		Matrix(A, A);
-		B /= 2;
-	}
-
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			cout << answer[i][j]<<' ';
-		}
-		cout << '\n';
-	}
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << answer[i][j] << ' ';
+        }
+        cout << '\n';
+    }
 }
