f56c40d83dc9c6014856cea0824ad872c1b88022
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-10 14:57:05 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,55 +1,50 @@
 #include <iostream>
-
+#include <vector>
+#define DIV  1000000
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    vector<int> dp1(N);
+    vector<int> dp2(N);
 
-	int A;
-	int answer = 0;
-	int arrA[1000];
-	int arrB[1000];
-	int arrC[1000];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+        dp1[i] = 1;
+        dp2[i] = 1;
+    }
 
-	cin >> A;
-	for (int i = 0; i < A; i++)
-	{
-		cin >> arrA[i];
-	}
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (v[i] > v[j])
+            {
+                dp1[i] = max(dp1[i], dp1[j] + 1);
+            }
+        }
+    }
 
-	for (int i = 0; i < A; i++)
-	{
-		arrB[i] = 1;
-		for (int j = 0; j < i; j++)
-		{
-			if (arrA[i] > arrA[j])
-			{
-				arrB[i] = max(arrB[i], arrB[j] + 1);
-			}
-		}
-	}
+    for (int i = N-1; i >=0 ; i--)
+    {
+        for (int j = N-1; j > i; j--)
+        {
+            if (v[i] > v[j])
+            {
+                dp2[i] = max(dp2[i], dp2[j] + 1);
+            }
+        }
+    }
 
-	for (int i = A-1; i >= 0; i--)
-	{
-		arrC[i] = 1;
-		for (int j = A-1; j > i; j--)
-		{
-			if (arrA[i] > arrA[j])
-			{
-				arrC[i] = max(arrC[i], arrC[j] + 1);
-			}
-		}
-	}
-
-	for (int i = 0; i < A; i++)
-	{
-		int sum = arrB[i] + arrC[i];
-		if (sum > answer)
-		{
-			answer = sum;
-		}
-	}
-
-	cout << answer - 1;
+    int answer = 0;
+    for (int i = 0; i < N; i++)
+    {
+        answer = max(dp1[i] + dp2[i] - 1, answer);
+    }
+    cout << answer;
 }
