b718483609b10fe475b5df4249e179d6ddb3076d
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-12 11:24:36 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,80 +1,51 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#define INF 987654321;
+#define INF 987654321
 
 using namespace std;
-
-int n, m;
-int map[101][101];
-vector<pair<int, int>> v[101];
-
-void dijsktra(int start)
-{
-    priority_queue<pair<int, int>> pq;
-    pq.push({ 0, start });
-
-    int first = start;
-    while (!pq.empty())
-    {
-        int curDist = -pq.top().first;
-        int curLoc = pq.top().second;
-        pq.pop();
-        
-        if (curDist > map[start][curLoc])
-        {
-            continue;
-        }
-
-        for (int i = 0; i < v[curLoc].size(); i++)
-        {
-            if (map[first][v[curLoc][i].first] > curDist + v[curLoc][i].second)
-            {
-                map[first][v[curLoc][i].first] = curDist + v[curLoc][i].second;
-                pq.push({ -map[first][v[curLoc][i].first], v[curLoc][i].first });
-            }
-        }
-    }
-}
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    cin >> m;
+    int arr[101][101] = { 0, };
+    int n, m;
+    cin >> n >> m;
 
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            map[i][j] = INF;
+            arr[i][j] = INF;
         }
+        arr[i][i] = 0;
     }
 
     for (int i = 0; i < m; i++)
     {
-        int start, end, cost;
-        cin >> start >> end >> cost;
-        v[start].push_back(make_pair(end, cost));
+        int a, b, c;
+        cin >> a >> b >> c;
+        arr[a][b] = min(arr[a][b], c);
     }
 
-    for (int i = 1; i <= n; i++)
+    for (int k = 1; k <= n; k++)
     {
-        dijsktra(i);
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
+            }
+        }
     }
 
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (map[i][j] == 987654321 || i == j)
+            if (arr[i][j] == INF)
             {
-                cout << 0 << ' ';
+                arr[i][j] = 0;
             }
-            else
-            {
-                cout << map[i][j] << ' ';
-            }
+            cout << arr[i][j] << ' ';
         }
         cout << '\n';
     }
