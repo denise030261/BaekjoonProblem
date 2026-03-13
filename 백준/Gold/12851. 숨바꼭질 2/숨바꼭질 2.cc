55c904e99988ffd461a006415fca5c303e29609c
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-13 21:51:40 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 24 ms, Memory: 4340 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,67 +1,76 @@
 #include <iostream>
 #include <queue>
+#define INF 987654321
 
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int N, K;
-	int time = 987654321;
-	bool visited[100001] = { 0, };
-	int method = 0;
-	queue<pair<int, int>> q;
-	cin >> N >> K;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, K;
+    bool big = true;
+    cin >> N >> K;
 
-	q.push({ 0,N });
-	visited[N] = true;
-	while (!q.empty())
-	{
-		int curDist = q.front().second;
-		int curCost = q.front().first;
-		q.pop();
-		visited[curDist] = true;
+    if (N > K)
+    {
+        cout << N - K << ' ' << 1 << '\n';
+        return 0;
+    }
 
-		if (curDist == K)
-		{
-			if (curCost == time)
-			{
-				method++;
-			}
-			else if (curCost < time)
-			{
-				time = curCost;
-				method = 1;
-			}
-			visited[curDist] = false;
-			continue;
-		}
+    priority_queue<pair<int,int>> pq;
+    pq.push({ 0,N });
 
-		if (time > curCost + 1)
-		{
-			if (curDist - 1 >= 0)
-			{
-				if (!visited[curDist - 1])
-				{
-					q.push({ curCost + 1, curDist - 1 });
-				}
-			}
-			if (curDist + 1 <= 100000 )
-			{
-				if (!visited[curDist + 1])
-				{
-					q.push({ curCost + 1, curDist + 1 });
-				}
-			}
-			if (curDist * 2 <= 100000 )
-			{
-				if (!visited[curDist * 2])
-				{
-					q.push({ curCost + 1, curDist * 2 });
-				}
-			}
-		}
-	}
+    int answer_num = INF;
+    int answer_way = 0;
+    int visited[200001] = { 0, };
+    for (int i = 1; i <= 200000; i++)
+    {
+        visited[i] = INF;
+    }
+   
+    while (!pq.empty())
+    {
+        int num = pq.top().second;
+        int cost = -pq.top().first;
+        visited[num] = cost;
+        pq.pop();
+        
+        if (num == K)
+        {
+            if (cost == answer_num)
+            {
+                answer_way++;
+            }
+            else if (cost < answer_num)
+            {
+                answer_num = cost;
+                answer_way = 1;
+            }
+        }
+        if (cost+1 > answer_num)
+        {
+            continue;
+        }
 
-	cout << time << '\n' << method;
+        if (num < K)
+        {
+            if(visited[num*2]>=cost+1)
+            {
+                pq.push({ -(cost + 1), num * 2 });
+            }
+            if(visited[num+1]>=cost+1)
+            {
+                pq.push({ -(cost + 1), num + 1 });
+            }
+        }
+        if (num - 1 > 0)
+        {
+            if(visited[num-1]>=cost+1)
+            {
+                pq.push({ -(cost + 1), num - 1 });
+            }
+        }
+    }
+
+    cout << answer_num << '\n' << answer_way;
 }
