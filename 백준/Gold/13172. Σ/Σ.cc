9959ccc8778bf94bd45ed974cf5f80987c7631dc
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-14 11:11:16 +0900

    [Gold IV] Title: Σ, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.cc
+++ 백준/Gold/13172. Σ/Σ.cc
@@ -1,39 +1,60 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#define DIV 1000000007
 
 using namespace std;
 
+long long int calculate(int a,int b)
+{
+    long long int answer = 1;
+
+    if (a == 1)
+    {
+        return b;
+    }
+
+    if (a % 2 == 1)
+    {
+        answer = (answer * b)%DIV;
+    }
+    long long half = calculate(a / 2, b) % DIV;
+    answer = (answer *( (half * half) % DIV))%DIV;
+
+    return answer;
+}
+
+long long int gcd(long long int a, long long int b)
+{
+    long long int c;
+
+    while (b != 0)
+    {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+
+    return a;
+}
+
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	long long int mod = 1000000007;
-	long long int answer = 0;
-	int M;
-	cin >> M;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int M;
+    long long int answer = 0;
+    cin >> M;
 
-	for (int i = 0; i < M; i++)
-	{
-        int S, N;
+    for (int i = 0; i < M; i++)
+    {
+        int N, S;
         cin >> N >> S;
+        
+        long long int div = gcd(S, N);
+        N /= div;
+        S /= div;
 
-        long long int result = 1;
-        long long int base = N; 
-        long long int exp = mod - 2;
+        long long int result = calculate(DIV - 2, N) % DIV;
+        answer = (answer+((S * result) % DIV))% DIV;
+    }
 
-        while (exp > 0)
-        {
-            if (exp % 2 == 1)
-            {
-                result = (result * base) % mod;
-            }
-            base = (base * base) % mod; 
-            exp /= 2;
-        }
-
-        answer += (S * result) % mod; 
-        answer %= mod;
-	}
-
-	cout << answer;
+    cout << answer;
 }
