04b8ba63fd819ab99aff73f86f916dcb23beea45
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-21 13:32:45 +0900

    [Gold IV] Title: 연구소, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,90 +1,131 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <algorithm>
 
 using namespace std;
 
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
-int n, m;
-int c = 0;
-int map[8][8];
-int tmp[8][8];
+int N, M;
+int arr[8][8] = { 0, };
+int temp[8][8] = { 0, };
+vector<pair<int, int>> v; // 벽을 배치될 빈 공간
+vector<pair<int, int>> infection; // 바이러스 
+bool placed[8][8] = { 0, };
+bool visited[8][8] = { 0, };
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+int answer = 0;
 
-void mapCopy(int(*a)[8], int(*b)[8]) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            a[i][j] = b[i][j];
+void init()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            temp[i][j] = arr[i][j];
+            visited[i][j] = false;
+        }
+    }
 }
 
-void bfs() {
-    int spread[8][8];
-    mapCopy(spread, tmp);
-    queue<pair<int, int>> q;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            if (spread[i][j] == 2)            
-                q.push(make_pair(i, j));
+void bfs()
+{
+    init();
 
-    while (!q.empty()) {
+    queue<pair<int, int>> q;
+    for (int i = 0; i < infection.size(); i++)
+    {
+        int x = infection[i].first;
+        int y = infection[i].second;
+        q.push({ x,y });
+        visited[x][y] = true;
+    }
+
+    while (!q.empty())
+    {
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
 
-            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                if (spread[nx][ny] == 0) {
-                    spread[nx][ny] = 2;                    
-                    q.push(make_pair(nx, ny));        
+        for (int i = 0; i < 4; i++)
+        {
+            int next_x = x + dx[i];
+            int next_y = y + dy[i];
+
+            if (next_x >= 0 && next_x < N && next_y >= 0 && next_y < M)
+            {
+                if (temp[next_x][next_y] == 0 && !visited[next_x][next_y])
+                {
+                    //cout << next_x << ' ' << next_y << ' ' << temp[next_x][next_y] << ' ' << visited[next_x][next_y] << '\n';
+                    visited[next_x][next_y] = true;
+                    temp[next_x][next_y] = 2;
+                    q.push({ next_x,next_y });
                 }
             }
         }
     }
-    
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (spread[i][j] == 0)
-                cnt += 1;
+
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (temp[i][j] == 0)
+            {
+                sum++;
+            }
         }
     }
-    c = max(c, cnt);
-}
+    answer = max(sum, answer);
+} // 감염 및 계산
 
-void wall(int cnt) {
-    if (cnt == 3) {
+void place(int index, int length)
+{
+    if (length == 3)
+    {
+        //cout << '\n';
         bfs();
         return;
+    } // 벽 배치 완료
+
+    for (int i = index; i < v.size(); i++)
+    {
+        int x = v[i].first;
+        int y = v[i].second;
+        if (!placed[x][y])
+        {
+            //cout << x << ' ' << y << '\n';
+            placed[x][y] = true;
+            arr[x][y] = 1;
+            place(i + 1, length + 1);
+            arr[x][y] = 0;
+            placed[x][y] = false;
+        }
     }
+} // 빈 곳 벽 배치
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            if (tmp[i][j] == 0) {
-                tmp[i][j] = 1;
-                wall(cnt + 1);
-                tmp[i][j] = 0;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> arr[i][j];
+
+            if (arr[i][j] == 0)
+            {
+                v.push_back({ i,j });
             }
-}
-
-int main() {
-    scanf("%d %d", &n, &m);
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            scanf("%d", &map[i][j]);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (map[i][j] == 0) {
-                mapCopy(tmp, map);
-                tmp[i][j] = 1;
-                wall(1);
-                tmp[i][j] = 0;
+            else if (arr[i][j] == 2)
+            {
+                infection.push_back({ i,j });
             }
         }
     }
-    printf("%d\n", c);
 
+    place(0, 0);
+
+    cout << answer;
 }
