2ea299159d89a7a57db5a88744cbf80a01ea5d71
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-23 12:45:41 +0900

    [Gold IV] Title: 서강그라운드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.cc
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.cc
@@ -1,60 +1,64 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#define INF 987654321;
 
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int n, m, r;
-	int t[101] = { 0, };
-	int item[101] = { 0, };
-	int answer = 0;
-	vector<pair<int, int>> v[101];
-	priority_queue<pair<int, int>> pq;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, m, r;
+    int arr[101][101] = { 0, };
+    int cost[101] = { 0, };
+    cin >> n >> m >> r;
 
-	cin >> n >> m >> r;
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> t[i];
-		item[i] = t[i];
-	}
-	for (int i = 0; i < r; i++)
-	{
-		int start, end, cost;
-		cin >> start >> end >> cost;
-		v[start].push_back({ end,cost });
-		v[end].push_back({ start,cost });
-	}
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> cost[i];
+    } // 아이템 수 입력
 
-	for (int i = 1; i <= n; i++)
-	{
-		pq.push({ 0, i });
-		int sum = 0;
-		for (int j = 1; j <= n; j++)
-		{
-			item[j] = t[j];
-		}
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            arr[i][j] = INF;
+            arr[j][i] = INF;
+        }
+        arr[i][i] = 0;
+    }
 
-		while (!pq.empty())
-		{
-			int curDist = -pq.top().first;
-			int curLoc = pq.top().second;
-			sum += item[curLoc];
-			item[curLoc] = 0;
-			pq.pop();
+    for (int i = 0; i < r; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        arr[a][b] = c;
+        arr[b][a] = c;
+    } // 길 입력
 
-			for (int i = 0; i < v[curLoc].size(); i++)
-			{
-				if (curDist + v[curLoc][i].second <= m)
-				{
-					pq.push({ -(curDist + v[curLoc][i].second) ,v[curLoc][i].first });
-				}
-			}
-		}
-		answer = max(answer, sum);
-	}
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
+            }
+        }
+    } // 플로이드
 
-	cout << answer;
+    int answer = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        for (int j = 1; j <= n; j++)
+        {
+            if (arr[i][j] <= m)
+            {
+                sum += cost[j];
+            }
+        }
+        
+        answer = max(answer, sum);
+    }
+
+    cout << answer;
 }
