8d298b62807f8632ec04e47f10742d8d76cdf531
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-24 13:59:03 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 44 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -1,136 +1,180 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+#include <queue>
 
 using namespace std;
 
-int map[100][100] = { 0, };
-int diffusionMap[100][100] = { 0, };
+int R, C, T;
+int arr[50][50] = { 0, }; // -1은 공기청정기
+int diff_temp[50][50] = { 0, };
+int blow_temp[50][50] = { 0, };
+int clock_R = -1, cclock_R = -1; // 시계, 반시계 행
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
 
-void diffusion(int R, int C) 
+void blow(int x,int y,bool clock)
 {
-    for (int i = 0; i < R; i++) 
+    blow_temp[x][y + 1] = 0;
+    for (int i = 1; i < C-1; i++)
     {
-        for (int j = 0; j < C; j++) 
+        blow_temp[x][i + 1] = diff_temp[x][i];
+    }
+
+    if (clock)
+    {
+        for (int i = x; i < R-1; i++)
         {
-            if (map[i][j] > 0) 
-            {
-                int dust = map[i][j] / 5;
-                int direction = 0;
-                if (i > 0 && map[i - 1][j] != -1) 
-                {
-                    diffusionMap[i - 1][j] += dust;
-                    direction++;
-                }
-                if (i < R - 1 && map[i + 1][j] != -1) 
-                {
-                    diffusionMap[i + 1][j] += dust;
-                    direction++;
-                }
-                if (j > 0 && map[i][j - 1] != -1) 
-                {
-                    diffusionMap[i][j - 1] += dust;
-                    direction++;
-                }
-                if (j < C - 1 && map[i][j + 1] != -1) 
-                {
-                    diffusionMap[i][j + 1] += dust;
-                    direction++;
-                }
-                diffusionMap[i][j] += map[i][j] - dust * direction;
-            }
+            blow_temp[i + 1][C - 1] = diff_temp[i][C - 1];
+        }
+    }
+    else
+    {
+        for (int i = x; i >= 1; i--)
+        {
+            blow_temp[i - 1][C - 1] = diff_temp[i][C - 1];
+        }
+    }
+
+    if (clock)
+    {
+        for (int i = C - 1; i >= 1; i--)
+        {
+            blow_temp[R - 1][i - 1] = diff_temp[R - 1][i];
+        }
+    }
+    else
+    {
+        for (int i = C - 1; i >= 1; i--)
+        {
+            blow_temp[0][i - 1] = diff_temp[0][i];
+        }
+    }
+
+    if (clock)
+    {
+        for (int i = R-1; i >= x+2; i--)
+        {
+            blow_temp[i - 1][0] = diff_temp[i][0];
+        }
+    }
+    else
+    {
+        for (int i = 0; i <= x-2; i++)
+        {
+            blow_temp[i + 1][0] = diff_temp[i][0];
         }
     }
 }
 
-void purify(int R, int C, pair<int, int> up, pair<int, int> down) 
+void diffusion()
 {
-    for (int i = up.first - 1; i > 0; i--)
+    queue<pair<int, int>> q;
+    for (int i = 0; i < R; i++)
     {
-        map[i][0] = map[i - 1][0];
-    }
-    for (int j = 0; j < C - 1; j++)
-    {
-        map[0][j] = map[0][j + 1];
-    }
-    for (int i = 0; i < up.first; i++)
-    {
-        map[i][C - 1] = map[i + 1][C - 1];
-    }
-    for (int j = C - 1; j > 1; j--)
-    {
-        map[up.first][j] = map[up.first][j - 1];
-    }
-    map[up.first][1] = 0;
+        for (int j = 0; j < C; j++)
+        {
+            diff_temp[i][j] = 0;
 
-    for (int i = down.first + 1; i < R - 1; i++)
-    {
-        map[i][0] = map[i + 1][0];
+            if (arr[i][j] != -1 && arr[i][j] != 0)
+            {
+                q.push({ i,j });
+            }
+        }
     }
-    for (int j = 0; j < C - 1; j++)
-    {
-        map[R - 1][j] = map[R - 1][j + 1];
-    }
-    for (int i = R - 1; i > down.first; i--)
-    {
-        map[i][C - 1] = map[i - 1][C - 1];
-    }
-    for (int j = C - 1; j > 1; j--)
-    {
-        map[down.first][j] = map[down.first][j - 1];
-    }
-    map[down.first][1] = 0;
-}
 
-int main() 
+    diff_temp[cclock_R][0] = -1;
+    diff_temp[clock_R][0] = -1;
+
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        int value = arr[x][y];
+        int diff_val = value / 5;
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int next_x = x + dx[i];
+            int next_y = y + dy[i];
+            if (next_x >= 0 && next_x<R && next_y >= 0 && next_y<C)
+            {
+                if (diff_val != 0 && diff_temp[next_x][next_y]!=-1)
+                {
+                    diff_temp[next_x][next_y] += diff_val;
+                    value -= diff_val;
+                }
+            }
+        }
+        if (value > 0)
+        {
+            diff_temp[x][y] += value;
+        }
+    }
+
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            blow_temp[i][j] = diff_temp[i][j];
+        }
+    }
+
+    blow(clock_R, 0, true);
+    blow(cclock_R,0,false);
+
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            arr[i][j] = blow_temp[i][j];
+        }
+    }
+    arr[cclock_R][0] = -1;
+    arr[clock_R][0] = -1;
+} // 확산
+
+int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int R, C, T;
-    vector<pair<int, int>> purifiers;
     cin >> R >> C >> T;
 
-    for (int i = 0; i < R; i++) 
+    for (int i = 0; i < R; i++)
     {
-        for (int j = 0; j < C; j++) 
+        for (int j = 0; j < C; j++)
         {
-            cin >> map[i][j];
-            if (map[i][j] == -1) 
+            cin >> arr[i][j];
+
+            if (arr[i][j] == -1)
             {
-                purifiers.push_back({ i, j });
+                if (cclock_R == -1)
+                {
+                    cclock_R = i;
+                } // 반시계
+                else 
+                {
+                    clock_R = i;
+                } // 시계
             }
         }
     }
 
     for (int i = 0; i < T; i++)
     {
-        fill(&diffusionMap[0][0], &diffusionMap[0][0] + sizeof(diffusionMap) / sizeof(int), 0);
-        diffusion(R, C);
-
-        for (int p = 0; p < R; p++)
-        {
-            for (int q = 0; q < C; q++)
-            {
-                if (map[p][q] != -1)
-                {
-                    map[p][q] = diffusionMap[p][q];
-                }
-            }
-        }
-
-        purify(R, C, purifiers[0], purifiers[1]);
+        diffusion();
     }
 
-    int sum = 0;
-    for (int i = 0; i < R; i++) 
+    int answer = 0;
+    for (int i = 0; i < R; i++)
     {
-        for (int j = 0; j < C; j++) 
+        for (int j = 0; j < C; j++)
         {
-            if (map[i][j] > 0) 
+            if (arr[i][j] != -1)
             {
-                sum += map[i][j];
+                answer += arr[i][j];
             }
         }
     }
-
-    cout << sum << '\n';
-    return 0;
+    cout << answer;
 }
