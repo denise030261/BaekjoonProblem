a1769a0a552b9561898fc70fb1f44dc5328f498d
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-26 16:38:23 +0900

    [Gold III] Title: 파티, Time: 116 ms, Memory: 6092 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,39 +1,84 @@
 #include <iostream>
+#include <queue>
+#include <vector>
+#define INF 987654321;
 
 using namespace std;
 
+int N, M, X;
+vector<pair<int, int>> road[1001];
+int cost[1001][1001] = { 0, };
+priority_queue<pair<int, int>> pq;
+
+void bfs(int start)
+{
+    pq.push({ -0,start });
+
+    while (!pq.empty())
+    {
+        int time = -pq.top().first;
+        int cur = pq.top().second;
+        pq.pop();
+
+        for (int i = 0; i < road[cur].size(); i++)
+        {
+            int next_node = road[cur][i].first;
+            int next_time = road[cur][i].second;
+            if (cost[start][next_node] > time + next_time)
+            {
+                cost[start][next_node] = time + next_time;
+                pq.push({ -(time + next_time),next_node });
+            }
+        }
+    }
+}
+
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int M, N, X;
-	cin >> N >> M >> X;
-	int graph[1001][1001] = { 0, };
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M >> X;
 
-	fill(&graph[0][0], &graph[0][0] + sizeof(graph) / sizeof(int), 987654321);
+    for (int i = 0; i < M; i++)
+    {
+        int start, end, time;
+        cin >> start >> end >> time;
+        road[start].push_back({ end,time });
+    }
 
-	for (int i = 0; i < M; i++)
-	{
-		int start, end, time;
-		cin >> start >> end >> time;
-		graph[start][end] = time;
-	}
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+        {
+            cost[i][j] = INF;
+        }
+        cost[i][i] = 0;
+    }
 
-	for (int k = 1; k <= N; k++)
-	{
-		graph[k][k] = 0;
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
-			}
-		}
-	}
+    for (int i = 1; i <= N; i++)
+    {
+        bfs(i);
+    }
 
-	int answer = 0;
-	for (int i = 1; i <= N; i++)
-	{
-		answer = max(answer, graph[i][X] + graph[X][i]);
-	}
-	cout << answer;
+    /*for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+        {
+            cout << cost[i][j] << ' ';
+        }
+        cout << '\n';
+    }*/
+    int answer = -1;
+    for (int i = 1; i <= N; i++)
+    {
+        if (i == X)
+        {
+            continue;
+        }
+
+        int sum = 0;
+        sum += cost[i][X];
+        sum += cost[X][i];
+        answer = max(sum, answer);
+    }
+    cout << answer;
 }
