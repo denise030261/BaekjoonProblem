f81936b069ee830c1e2a0354f35a5b9e01ef8e19
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-29 21:22:32 +0900

    [Gold V] Title: 회문, Time: 12 ms, Memory: 2552 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+bool correct(int start, int end, string str)
+{
+    while (start < end)
+    {
+        if (str[start] != str[end])
+        {
+            return false;
+        }
+
+        start++; end--;
+    }
+
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        string str;
+        cin >> str;
+
+        int end = str.length() - 1;
+        int start = 0;
+        
+        int circle = 0;
+        while (start<end && circle!=2)
+        {
+            if (str[start] != str[end])
+            {
+                if (circle == 1)
+                {
+                    circle = 2;
+                    break;
+                }// 또 틀림
+
+                bool possible = false;
+                if (str[start] == str[end - 1] && !possible)
+                {
+                    if(correct(start+1,end-2,str))
+                    {
+                        possible = true;
+                        end--;
+                        circle = 1;
+                    }
+                }
+                if (str[start + 1] == str[end] && !possible)
+                {
+                    if (correct(start + 2, end - 1, str))
+                    {
+                        possible = true;
+                        start++;
+                        circle = 1;
+                    }
+                }
+                
+                if(!possible)
+                {
+                    circle = 2;
+                }
+            }
+
+            start++; end--;
+        }
+        cout << circle << '\n';
+    }
+}
