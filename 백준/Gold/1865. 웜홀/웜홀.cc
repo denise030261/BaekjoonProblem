f07051562fd026810bd65b276e08d9e06e0f6a11
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-03-30 13:41:08 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -1,82 +1,81 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <cstring>
-#include <climits>
+#include <vector>
+#define INF 987654321
 
 using namespace std;
 
-vector<pair<int,pair<int, int>>> edges; // 다음 위치 및 비용
-int dist[501] = { 0, };
-int TC, N, M, W;
-
-bool bellmanFord()
-{
-	dist[1] = 0;
-	
-	for (int i = 1; i < N; i++)
-	{
-		for (int j = 0; j < edges.size(); j++)
-		{
-			int from = edges[j].first;
-			int to = edges[j].second.first;
-			int cost = edges[j].second.second;
-
-			if (dist[to] > dist[from] + cost)
-			{
-				dist[to] = dist[from] + cost;
-			}
-		}
-	}
-	
-	for (int i = 0; i < edges.size(); i++)
-	{
-		int from = edges[i].first;
-		int to = edges[i].second.first;
-		int cost = edges[i].second.second;
-
-		if (dist[to] > dist[from] + cost)
-		{
-			return true;
-		}
-	}
-
-	return false;
-}
-
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> TC;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int TC;
+    cin >> TC;
 
-	while (TC != 0)
-	{
-		edges.clear();
-		for (int i = 1; i <= 500; i++)
-		{
-			dist[i] = 1e9;
-		}
+    while (TC != 0)
+    {
+        int N, M, W; // N은 지점 수, M은 도로 개수, W은 웜홀의 개수
+        int dist[501] = { 0, };
+        vector<pair<pair<int,int>, int>> v;
+        cin >> N >> M >> W;
+        for (int i = 0; i < M; i++)
+        {
+            int S, E, T; // S, E는 연결 지점 번호, T는 시간
+            cin >> S >> E >> T;
+            v.push_back({ {S,E},T });
+            v.push_back({ {E,S},T });
+        } // 도로 정보
 
-		cin >> N >> M >> W;
-		for (int i = 0; i < M; i++)
-		{
-			int S, E, T;
-			cin >> S >> E >> T;
-			edges.push_back({ S,{E,T} });
-			edges.push_back({ E,{S,T} });
-		}
-		for (int i = 0; i < W; i++)
-		{
-			int S, E, T;
-			cin >> S >> E >> T;
-			edges.push_back({ S,{ E,-T } });
-		}
+        for (int i = 0; i < W; i++)
+        {
+            int S, E, T;
+            cin >> S >> E >> T;
+            v.push_back({ {S,E},-T });
+        }
 
-		if (bellmanFord())
-			cout << "YES\n";
-		else
-			cout << "NO\n";
+        for (int i = 1; i <= N; i++)
+        {
+            dist[i] = INF;
+        }
+        dist[1] = 0;
 
-		TC--;
-	}
+        for (int i = 1; i < N; i++)
+        {
+            for (int j = 0; j < v.size(); j++)
+            {
+                int from = v[j].first.first;
+                int to = v[j].first.second;
+                int cost = v[j].second;
+
+                if (dist[to] > dist[from] + cost)
+                {
+                    dist[to] = dist[from] + cost;
+                }
+            }
+        }
+
+        bool possible = false;
+        for (int j = 0; j < v.size(); j++)
+        {
+            int from = v[j].first.first;
+            int to = v[j].first.second;
+            int cost = v[j].second;
+
+            if (dist[to] > dist[from] + cost)
+            {
+                possible = true;
+                break;
+            }
+        }
+
+        if (possible)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+
+        TC--;
+    }
 }
