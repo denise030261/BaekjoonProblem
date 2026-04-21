ccdbfc0f5142a107715c2c6ed0ecc89de604b575
denise030261 <83278071+denise030261@users.noreply.github.com>
2026-04-22 00:35:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.26 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        a--;
+        b--;
+        
+        while(a!=b)
+        {
+            a/=2;
+            b/=2;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
